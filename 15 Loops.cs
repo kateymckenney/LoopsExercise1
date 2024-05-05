8ecf2b57f07ec9da81a6ad984998909ad8c351ea
Katey McKenney <mckenneykatey@gmail.com>
2024-05-05 12:53:51 -0500

    typed loop 15+ times

---
+++ 15 Loops.cs
@@ -0,0 +1,94 @@
+for(int i = 0, i < 5; i++)
+{
+Console.WriteLine($"{i}");
+}
+
+for(int i = 0, i < 5, i++)
+{
+Console.WriteLine($"{i}");
+}
+
+for(int i = 0, i < 5, i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0, i < 5, i++)
+{
+Console.WriteLine($"{i}");
+}
+
+for(int i = 0, i < 5, i++)
+{
+Console.WriteLine($"{i}");
+}
+
+for(int i = 0, i < 5, i++)
+{
+Console.WriteLine($"{i}");
+}
+
+for(int i = 0, i < 5, i++)
+{
+Console.WriteLine($"{i}");
+}
+
+for(int i = 0, i < 5, i++)
+{
+Console.WriteLine($"{i}");
+}
+
+for (int i = 0, i < 5, i++)
+{
+Console.WriteLine($"{i}");
+}
+
+for (int i = 0, i < 5, i++)
+{
+Console.WriteLine($"{i}")
+}
+
+for (int i = 0, i < 5, i++)
+{
+Console.WriteLine($"{i}")
+}
+
+for (int i = 0, 1 < 5, i++)
+{
+Console.WriteLine($"{i}");
+}
+
+for (int i = 0, i < 5, i++)
+{
+Console.WriteLine($"{i}")
+}
+
+for (int i = 0, i < 5, i++)
+{
+Console.WriteLine($"{i}");
+}
+
+for (int i = 0, in < 5, i++)
+{
+Console.WriteLine($"{i}");
+}
+
+for(int i = 0, i < 5, i++)
+{
+Console.WriteLine($"{i}");
+}
+
+for (int i = 0, i < 5, i++);
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0, i < 5, i++)
+{
+Console.WriteLine($"{i}");
+}
+
+for (int i = 0, i < 5, i++)
+{
+Console.WriteLine($"{i}");
+}
